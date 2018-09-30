5cc2a091450e4233bf8fa0f63c85f316e8d18d2d
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2018-10-01 01:35:10 +0300

    First 2 exercises

---
+++ go/gigasecond/gigasecond.go
@@ -0,0 +1,9 @@
+package gigasecond
+
+// import path for the time package from the standard library
+import "time"
+
+// AddGigasecond returns the given time + 10^9 seconds
+func AddGigasecond(t time.Time) time.Time {
+	return t.Add(time.Second * 1000000000)
+}
---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,6 @@
+package greeting
+
+// HelloWorld returns the hello world string
+func HelloWorld() string {
+	return "Hello, World!"
+}
