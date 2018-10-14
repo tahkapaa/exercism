94326fba7321916ab8d5a7efae1ea3ea28df7d7b
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2018-10-14 12:26:38 +0300

    Use Scientific notation

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -5,5 +5,5 @@ import "time"
 
 // AddGigasecond returns the given time + 10^9 seconds
 func AddGigasecond(t time.Time) time.Time {
-	return t.Add(time.Second * 1000000000)
+	return t.Add(time.Second * 1e9)
 }
