b82ec853bc9d16d077038f4eda3d6885cbff5ea0
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2018-10-14 21:14:16 +0300

    Resolve leap

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,15 +1,20 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
+/*
+Package leap given a year, report if it is a leap year.
 
-// Package leap should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+The tricky thing here is that a leap year in the Gregorian calendar occurs:
+
+```text
+on every year that is evenly divisible by 4
+  except every year that is evenly divisible by 100
+    unless the year is also evenly divisible by 400
+```
+
+For example, 1997 is not a leap year, but 1996 is.  1900 is not a leap
+year, but 2000 is.
+*/
 package leap
 
-// IsLeapYear should have a comment documenting it.
-func IsLeapYear(int) bool {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return false
+// IsLeapYear returns true if the given year is leap year.
+func IsLeapYear(year int) bool {
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
